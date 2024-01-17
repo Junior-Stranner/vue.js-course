e9c9a3859fc6f92b422e15df0d775456418b1d84
Junior-Stranner <Ju-stranner@outlook.com>
2024-01-17 10:30:58 -0300

    vue.js first steps

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<!--Script-->
+<script setup>
+  
+  //ariáveis
+  let nome = 'Junior';
+  let idade = 22;
+
+</script>
+
+<!--HTML-->
+<template>
+    <p>{{"olá "+nome+"idade "+idade}}</p>
+</template>
