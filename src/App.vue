e5c5ae714f4179afe255095650596f1df137243e
Junior-Stranner <Ju-stranner@outlook.com>
2024-01-17 10:32:47 -0300

    testing

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,14 @@
   //ariáveis
   let nome = 'Junior';
   let idade = 22;
+  let numero1 = 10;
+  let numero2 = 5;
 
 </script>
 
 <!--HTML-->
 <template>
     <p>{{"olá "+nome+"idade "+idade}}</p>
+    <p>{{numero1 + numero2 }}</p>
+
 </template>
